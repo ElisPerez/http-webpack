eee6e2ae9e91f16416b1a31a4c9c3444f95c1212
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-11-20 23:36:57 -0500

    Response, Data, Destructuring Data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/style.css';
 
-console.log('hello world');
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+
+/* ¿Qué es la RESPONSE? */
+// fetch(jokeUrl).then(response => {
+//   console.log(response);
+// });
+
+/* La DATA */
+// fetch(jokeUrl).then(response => {
+//   response.json().then(data => {
+//     console.log(data);
+//   });
+// });
+
+/* Destructuring the DATA */
+fetch(jokeUrl).then(response => {
+  response.json().then(({ id, value }) => {
+    console.log(id);
+    console.log(value);
+  });
+});
